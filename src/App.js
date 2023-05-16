91d2739105f688993f59f037b21def946e8a3149
Midori Takahashi <mintgreen7programming@gmail.com>
2023-05-16 19:29:11 +0900

    FIX:~最後のお掃除

--- src/App.js
+++ src/App.js
@@ -53,14 +53,37 @@ function Board( { xIsNext, squares, onPlay } ) {
 }
 
 export default function Game() {
-  const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
-  const currentSquares = history[history.length - 1];
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares) {
-    setHistory([...history , nextSquares]);
-    setXIsNext(!xIsNext);
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+    console.log(history, nextSquares);
   }
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+    console.log(nextMove);
+  }
+
+  const moves = history.map((squares, move) => {
+    let discription;
+    if (move > 0) {
+      discription = 'Go to move #' + move;
+    } else {
+      discription = 'Go to game start';
+    }
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{discription}</button>
+      </li>
+    );
+  });
+
 
   return (
     <div className="game">
@@ -68,7 +91,7 @@ export default function Game() {
         <Board  xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className="game-info">
-        <ol>{/* TODO */}</ol>
+        <ol>{moves}</ol>
       </div>
     </div>
   );
